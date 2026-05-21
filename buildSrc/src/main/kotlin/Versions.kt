6be8a02a3c5120276ba1f7f0cbc1195c7b7897bd
Clement Bobin <119869686+ClementBobin@users.noreply.github.com>
2026-05-21 12:15:25 +0000

    fix: update jvmTarget version to 21 for compatibility

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlin = "2.2.0"
-    const val jvmTarget = "17"
+    const val jvmTarget = "21"
     const val group = "io.github.clementbobin"
     const val libraryVersion = "0.1.0"
     const val dokka = "2.0.0"
