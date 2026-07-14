3accb538db465773ac1b0675098b5beb7bc33dc7
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-07-14 12:52:48 +0200

    feat: update exception message in KCacheConfig for clarity on network failures

--- android/src/main/kotlin/dev/kindling/android/http/KCacheConfig.kt
+++ android/src/main/kotlin/dev/kindling/android/http/KCacheConfig.kt
@@ -145,7 +145,7 @@ internal fun createCachePlugin(config: KCacheConfig) =
                 }
                 return call
             }
-            return cached?.let { forwardToMock(request) } ?: throw (exception ?: IllegalStateException("Unknown failure"))
+            return cached?.let { forwardToMock(request) } ?: throw (exception ?: IllegalStateException("Network failure"))
         }
 
         on(Send) { request ->
