8fecfd120c51a0e8c92ca9fbf960d9737c5e0a4c
Clement Bobin <119869686+ClementBobin@users.noreply.github.com>
2026-08-25 09:50:34 +0000

    fix: comment out unused Bouncy Castle dependency for clarity

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
     implementation("org.bouncycastle:bcpkix-jdk18on:${Versions.castle}")
     implementation("org.bouncycastle:bcprov-jdk18on:${Versions.castle}")
-    implementation("org.bouncycastle:bcpqc-jdk18on:${Versions.castle}")
+    // implementation("org.bouncycastle:bcpqc-jdk18on:${Versions.castle}")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junit5}")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}")
